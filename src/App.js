32286d8d58b3e3aa657ed0aa4860d99bd7d7fcb1
SawantVinit22 <sawantvinit22@gmail.com>
2021-12-20 22:04:36 +0530

    Changes in react app.js by Vinit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import AmanComponent from './Components/Aman';
-import DeepanshuComponent  from './Components/Deepanshu';
+import DeepanshuComponent from './Components/Deepanshu';
+import VinitComp from './Components/Vinit';
 
 function App() {
   return (
-  <div className="App">
-	  <h2>React App</h2>
-      <AmanComponent/>
-      <DeepanshuComponent/>
-  </div>
+    <div className="App">
+      <h2>React App</h2>
+      <AmanComponent />
+      <DeepanshuComponent />
+      <VinitComp />
+    </div>
   );
 }
 
